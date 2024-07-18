816fad9e46530f9ac22a3cadf568b5c7b8a5afc1
Hyunsoo_kim <red_panda119@naver.com>
2024-07-18 13:33:41 +0900

    Feat : CURD 게시판 모델, 시드데이터 생성, 삽입

---
+++ data/mock.js
@@ -0,0 +1,50 @@
+const seedNotice = [
+  {
+    subCategory: "All",
+    title: "공지사항 게시판 테스트",
+    content: "공지사항 게시판 테스트 입니다."
+  }
+];
+
+const seedNews = [
+  {
+    subCategory: "국내",
+    title: "뉴스 게시판 테스트",
+    content: "뉴스 게시판 테스트 입니다."
+  }
+];
+
+const seedBoard = [
+  {
+    subCategory: "일반",
+    writter: "운영자",
+    title: "자유게시판 테스트",
+    content: "자유게시판 테스트 입니다."
+  }
+];
+
+const seedPromote = [
+  {
+    subCategory: "밴드홍보",
+    wirtter: "운영자",
+    title: "프로모트 게시판 테스트",
+    content: "프로모트 게시판 테스트 입니다."
+  }
+];
+
+const seedJob = [
+  {
+    subCategory: "구인",
+    writter: "운영자",
+    title: "잡 게시판 테스트",
+    content: "잡 게시판 테스트 입니다."
+  }
+]
+
+module.exports = {
+  seedNotice,
+  seedNews,
+  seedBoard,
+  seedPromote,
+  seedJob
+};
---
+++ model/bbs.js
@@ -0,0 +1,195 @@
+const mongoose = require("mongoose");
+
+const { CommentSchema } = require("./comment.js");
+
+const NoticePostSchema = new mongoose.Schema(
+  {
+    subCategory: {
+      type: String,
+      enum: ["All"],
+      trim: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      minLength: 2,
+      maxLength: 40,
+      trim: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    recommend: {
+      type: Number,
+      default: 0,
+    },
+    content: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 5000,
+      trim: true,
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const NewsPostSchema = new mongoose.Schema(
+  {
+    subCategory: {
+      type: String,
+      enum: ["국내", "국외", "All"],
+      trim: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    recommend: {
+      type: Number,
+      default: 0,
+    },
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    comment: [CommentSchema]
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const BoardPostSchema = new mongoose.Schema(
+  {
+    subCategory: {
+      type: String,
+      enum: ["일반", "녹음", "팁", "All"],
+      trim: true,
+    },
+    writter: {
+      type: String,
+      minLength: 2,
+      maxLength: 8,
+    },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    recommend: {
+      type: Number,
+      default: 0,
+    },
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    comment: [CommentSchema]
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const PromotePostSchema = new mongoose.Schema(
+  {
+    subCategory: {
+      type: String,
+      enum: ["밴드홍보", "앨범홍보", "재즈바홍보", "All"],
+      trim: true,
+    },
+    writter: {
+      type: String,
+      minLength: 2,
+      maxLength: 8,
+    },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    recommend: {
+      type: Number,
+      default: 0,
+    },
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    comment: [CommentSchema]
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const JobPostSchema = new mongoose.Schema(
+  {
+    subCategory: {
+      type: String,
+      enum: ["구인", "구직", "All"],
+      trim: true,
+    },
+    writter: {
+      type: String,
+      minLength: 2,
+      maxLengthL: 8,
+    },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    recommend: {
+      type: Number,
+      default: 0,
+    },
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    comment: [CommentSchema]
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const NoticePost = mongoose.model("NoticePost", NoticePostSchema);
+const NewsPost = mongoose.model("NewsPost", NewsPostSchema);
+const BoardPost = mongoose.model("BoardPost", BoardPostSchema);
+const PromotePost = mongoose.model("PromotePost", PromotePostSchema);
+const JobPost = mongoose.model("JobPost", JobPostSchema);
+
+module.exports = {
+  NoticePost,
+  NewsPost,
+  BoardPost,
+  PromotePost,
+  JobPost,
+};
