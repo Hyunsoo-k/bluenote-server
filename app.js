8e08b547465ae2d7f8d102efe25594f7ceb9c74b
Hyunsoo_kim <red_panda119@naver.com>
2024-07-20 12:31:25 +0900

    Feat : app.route() 적용

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ mongoose.connect(process.env.DATABASE_URL).then(() => console.log("Connected to 
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: ["http://localhost:3000", "https://bluenote-server.onrender.com"]}));
+app.use(cors({ origin: ["http://localhost:3000", "https://bluenote-server.onrender.com"] }));
 
 const asyncHandler = (handler) => {
   return async (req, res) => {
@@ -32,113 +32,131 @@ const getModel = (mainCategory) => {
     job: JobPost,
   };
   return modelMap[mainCategory];
-}
+};
 
-// 게시판
+// 게시글 목록 조회, 게시글 등록
 
-app.get(
-  "/bbs/:main_category",
-  asyncHandler(async (req, res) => {
-    const posts = await getModel(req.params.main_category).find().sort({ createdAt: -1 });
-    const count = await getModel(req.params.main_category).countDocuments();
-    const postsData = {
-      posts: posts,
-      count: count
-    }
-    res.send(postsData);
-  })
-);
+app.route("/bbs/:main_category")
+  .get(
+    asyncHandler(async (req, res) => {
+      const posts = await getModel(req.params.main_category).find().sort({ createdAt: -1 });
+      const count = await getModel(req.params.main_category).countDocuments();
+      const postsData = {
+        posts: posts,
+        count: count,
+      };
+      res.send(postsData);
+    })
+  )
+  .post(
+    asyncHandler(async (req, res) => {
+      const newPost = await getModel(req.params.main_category).create(req.body);
+      res.status(201).send(newPost);
+    })
+  );
 
-app.post(
-  "/bbs/:main_category",
-  asyncHandler(async (req, res) => {
-    const newPost = await getModel(req.params.main_category).create(req.body);
-    res.status(201).send(newPost);
-  })
-);
+// 게시글 조회, 게시글 수정, 게시글 삭제
 
-app.patch(
-  "/bbs/:main_category/:id",
-  asyncHandler(async (req, res) => {
-    const post = await getModel(req.params.main_category).findById(req.params.id);
-    if (post) {
-      Object.keys(req.body).forEach((key) => {
-        post[key] = req.body[key];
-      });
-      await post.save();
-      res.send(post);
-    } else {
-      res.status(404).send({ message: "Cannot find given id." });
-    }
-  })
-);
-
-app.delete(
-  "/bbs/:main_category/:id",
-  asyncHandler(async (req, res) => {
-    const post = await getModel(req.params.main_category).findByIdAndDelete(req.params.id);
-    if (post) {
-      res.sendStatus(204);
-    } else {
-      res.status(404).send({ message: "Cannot find given id." });
-    }
-  })
-);
-
-// 게시판 댓글
-
-app.post(
-  "/bbs/:main_category/:id/comment",
-  asyncHandler(async (req, res) => {
-    const post = await getModel(req.params.main_category).findById(req.params.id);
-    if (post) {
-      post.comment.push(req.body);
-      await post.save();
-      res.status(201).send(post);
-    } else {
-      res.status(404).send({ message: "Cannot find given id." });
-    }
-  })
-);
-
-app.patch(
-  "/bbs/:main_category/:id/comment/:comment_id",
-  asyncHandler(async (req, res) => {
-    const post = await getModel(req.params.main_category).findById(req.params.id);
-    if (post) {
-      const comment = post.comment.id(req.params.comment_id);
-      if (comment) {
+app.route("/bbs/:main_category/:id")
+  .get(
+    asyncHandler(async (req, res) => {
+      const post = await getModel(req.params.main_category).findById(req.params.id);
+      if (post) {
+        res.send(post);
+      } else {
+        res.status(404).send({ message: "Cannot find given id." });
+      }
+    })
+  )
+  .patch(
+    asyncHandler(async (req, res) => {
+      const post = await getModel(req.params.main_category).findById(req.params.id);
+      if (post) {
         Object.keys(req.body).forEach((key) => {
-          comment[key] = req.body[key];
+          post[key] = req.body[key];
         });
         await post.save();
-        res.send(comment);
+        res.send(post);
       } else {
-        res.status(404).send({ message: "Cannot find comment with given id." });
+        res.status(404).send({ message: "Cannot find given id." });
       }
-    } else {
-      res.status(404).send({ message: "Cannot find post with given id." });
-    }
-  })
-);
-
-app.delete(
-  "/bbs/:main_category/:id/comment/:comment_id",
-  asyncHandler(async (req, res) => {
-    const post = await getModel(req.params.main_category).findById(req.params.id);
-    if (post) {
-      const comment = post.comment.id(req.params.comment_id);
-      if (comment) {
-        post.comment.pull(comment._id);
-        await post.save();
+    })
+  )
+  .delete(
+    asyncHandler(async (req, res) => {
+      const post = await getModel(req.params.main_category).findByIdAndDelete(req.params.id);
+      if (post) {
         res.sendStatus(204);
       } else {
-        res.status(404).send({ message: "Cannot find comment with given id." });
+        res.status(404).send({ message: "Cannot find given id." });
       }
-    } else {
-      res.status(404).send({ message: "Cannot find given id." });
-    }
-  })
-);
+    })
+  );
+
+// 게시글 댓글 목록 조회, 게시글 댓글 등록
+
+app.route("/bbs/:main_category/:id/comment")
+  .get(
+    asyncHandler(async (req, res) => {
+      const post = await getModel(req.params.main_category).findById(req.params.id);
+      if (post.comment) {
+        res.send(post.comment);
+      } else {
+        res.status(404).send({ message: "there no comments." });
+      }
+    })
+  )
+  .post(
+    asyncHandler(async (req, res) => {
+      const post = await getModel(req.params.main_category).findById(req.params.id);
+      if (post) {
+        post.comment.push(req.body);
+        await post.save();
+        res.status(201).send(post);
+      } else {
+        res.status(404).send({ message: "Cannot find given id." });
+      }
+    })
+  )
+
+// 게시글 댓글 수정, 게시글 댓글 삭제
+
+app.route("/bbs/:main_category/:id/comment/:comment_id")
+  .patch(
+    asyncHandler(async (req, res) => {
+      const post = await getModel(req.params.main_category).findById(req.params.id);
+      if (post) {
+        const comment = post.comment.id(req.params.comment_id);
+        if (comment) {
+          Object.keys(req.body).forEach((key) => {
+            comment[key] = req.body[key];
+          });
+          await post.save();
+          res.send(comment);
+        } else {
+          res.status(404).send({ message: "Cannot find comment with given id." });
+        }
+      } else {
+        res.status(404).send({ message: "Cannot find post with given id." });
+      }
+    })
+  )
+  .delete(
+    asyncHandler(async (req, res) => {
+      const post = await getModel(req.params.main_category).findById(req.params.id);
+      if (post) {
+        const comment = post.comment.id(req.params.comment_id);
+        if (comment) {
+          post.comment.pull(comment._id);
+          await post.save();
+          res.sendStatus(204);
+        } else {
+          res.status(404).send({ message: "Cannot find comment with given id." });
+        }
+      } else {
+        res.status(404).send({ message: "Cannot find given id." });
+      }
+    })
+  );
 
 app.listen(process.env.PORT || 3000, () => console.log("Server Started"));
