e62be4a04d709b453bfac476e5b96ae2519d4de7
Hyunsoo_kim <red_panda119@naver.com>
2024-09-06 17:04:01 +0900

    Feat : user 스키마 필드 추가

--- routes/user.js
+++ routes/user.js
@@ -6,19 +6,46 @@ const { asyncHandler } = require("../utils/asyncHandler.js");
 
 const router = express.Router();
 
-router.route("/").get(
-  asyncHandler(async (req, res) => {
-    const { payload } = getTokenAndPayload(req);
-    const user = await User.findById(payload._id).lean();
+router
+  .route("/")
+  .get(
+    asyncHandler(async (req, res) => {
+      const { payload } = getTokenAndPayload(req);
+      const user = await User.findById(payload._id).lean();
 
-    return res.send({
-      _id: user._id,
-      email: user.email,
-      nickname: user.nickname,
-      createdAt: user.createdAt,
-      role: user.role,
-    });
-  })
-);
+      return res.send({
+        _id: user._id,
+        email: user.email,
+        nickname: user.nickname,
+        createdAt: user.createdAt,
+        role: user.role,
+      });
+    })
+  )
+  .patch(
+    asyncHandler(async (req, res) => {
+      const { nickname } = req.body;
+      const { payload } = getTokenAndPayload(req);
+      const isNicknameExist = await User.findOne({ nickname });
+
+      if (!payload) {
+        return res.statue(401).send({ message: "Unauthorized." });
+      }
+
+      if (isNicknameExist) {
+        return res.status(409).send({ message: "이미 존재하는 닉네임 입니다." });
+      }
+
+      const user = await User.findByIdAndUpdate(payload._id, { nickname }, { new: true, runValidators: true }).lean();
+
+      return res.send({
+        _id: user._id,
+        email: user.email,
+        nickname: user.nickname,
+        createdAt: user.createdAt,
+        role: user.role,
+      });
+    })
+  );
 
 module.exports = router;
