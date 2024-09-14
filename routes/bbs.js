ba862e165b5e48693f6ed909ca01681203e138a6
Hyunsoo_kim <red_panda119@naver.com>
2024-09-14 13:40:02 +0900

    Feat : 게시글 본문 공백 비교시 비교 연산자 수정(2)

--- routes/bbs.js
+++ routes/bbs.js
@@ -130,7 +130,7 @@ router.route("/:mainCategory/post").post(
       return res.status(401).send({ message: "제목을 입력해 주세요."})
     }
 
-    if (req.body.content = "<p><br></p>") {
+    if (req.body.content === "<p><br></p>") {
       return res.statue(401).send({ message: "내용을 입력해 주세요." })
     };
 
