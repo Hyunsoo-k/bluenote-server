ec909650167932b121d98c9d351eca0a0cce49f0
Hyunsoo_kim <red_panda119@naver.com>
2024-09-29 23:53:43 +0900

    Feat : model맵핑 함수를 객체로 변경, 게시글 검색 기능 구현

--- dbUtils/edit.js
+++ dbUtils/edit.js
@@ -2,14 +2,17 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 const { NoticePost, NewsPost, BoardPost, PromotePost, JobPost } = require("../model/bbs.js");
-const { User } = require("../model/user.js");
 
 dotenv.config();
 mongoose.connect(process.env.DATABASE_URL).then(() => console.log("Connected to DB to edit"));
 
 async function renameCommentField() {
   try {
-    await User.updateMany({}, { profileImageUrl: "", part: "-" });
+    await NoticePost.updateMany({}, { recommend: [] });
+    await NewsPost.updateMany({}, { recommend: [] });
+    await BoardPost.updateMany({}, { recommend: [] });
+    await PromotePost.updateMany({}, { recommend: [] });
+    await JobPost.updateMany({}, { recommend: [] });
 
     console.log("Successfully add part, profileImageUrl Fields.");
   } catch (error) {
