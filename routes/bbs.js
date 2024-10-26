2d983ed810b70243e4b733d26971d86e9c351305
Hyunsoo_kim <red_panda119@naver.com>
2024-10-27 03:33:04 +0900

    Feat : 대댓 삭제시, 삭제된 댓글에 달린 마지막 대댓글이면 댓글도 삭제

--- routes/bbs.js
+++ routes/bbs.js
@@ -88,19 +88,19 @@ router
 
       if (!post) {
         return res.status(404).send({ message: "게시글을 찾을 수 없습니다." });
-      }
+      };
 
       if (!accessToken || post.writer._id.toString() !== payload._id) {
         return res.status(401).send({ message: "Unauthorized." });
-      }
+      };
 
       if (!req.body.title) {
         return res.status(400).send({ message: "제목을 입력해 주세요." });
-      }
+      };
 
       if (req.body.content === "<p><br></p>") {
         return res.status(400).send({ message: "내용을 입력해 주세요." });
-      }
+      };
 
       const editedPost = await modelMap[mainCategory]
         .findByIdAndUpdate(post_id, { $set: req.body }, { new: true })
