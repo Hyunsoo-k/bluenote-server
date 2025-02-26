fac4a1d178e520c122883f0c35ba7619fc7d504f
Hyunsoo_kim <red_panda119@naver.com>
2025-02-26 12:47:07 +0900

    Feat : recentSearch 모델 생성

---
+++ model/userRecentSearch.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const recentSearchSchema = new mongoose.Schema(
+  {
+    user: {
+      required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    content: {
+      required: true,
+      type: [{ type: String, trim: true }],
+      default: []
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const RecentSearch = mongoose.model("RecentSearch", recentSearchSchema);
+
+module.exports = { RecentSearch };
